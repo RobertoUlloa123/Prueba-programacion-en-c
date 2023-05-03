d7e85c38ede1c24fe0b477d781baba9d100c6b41
Roberto-Ulloa <111545962+RobertoUlloa123@users.noreply.github.com>
2023-05-03 15:22:21 -0500

    Update ejercicio1.c

--- ejercicio1.c
+++ ejercicio1.c
@@ -2,6 +2,20 @@
 
 int main()
 {
+    int n=0,n2=1,n3,numero,limite;
     
+    printf(" Ingrese un numero para hacer la serie Fibonacci \n");
+    scanf("%d",&numero);
+    printf (" %d + %d",%n,%n2);
+    for (int limite=n; limite<=n; limite++) 
+    {
+       
+        
+        n3=n+n2
+        n=n3+n2;
+         printf (" %d + %d +  " %n3, %n2, %n);
+         
+         
+    }
     return 0;
 }
